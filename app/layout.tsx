2761f94241d18e2e3738c48b253f3b006ec8a70f
ScofieldaCreep <tju_scofield@outlook.com>
2024-12-15 06:22:04 -0500

    add vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import localFont from 'next/font/local'
 
 import Header from '@/components/ui/header'
 import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+
 const inter = Inter({
 	subsets: ['latin'],
 	variable: '--font-inter',
@@ -58,6 +60,7 @@ export default function RootLayout({
 					<Header />
 					{children}
 					<Analytics />
+					<SpeedInsights />
 				</div>
 			</body>
 		</html>
